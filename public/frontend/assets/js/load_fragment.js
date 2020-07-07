6cdaf60c34cb2522815aff06d52bde9b6b3844a0
lawrence910426 <lawrence910426@gmail.com>
2020-07-07 15:07:32 +0800

    Tiny update

--- public/frontend/assets/js/load_fragment.js
+++ public/frontend/assets/js/load_fragment.js
@@ -1,12 +1,12 @@
 if($('#Nav-bar').html() == "") {
-    $.get("Fragments/nav.html", function(data){
+    $.get("nav.html", function(data){
         data = data.replace(/^.*?<div id="nav">(.*?)<\/div>.*?$/s, "$1");
         $('#Nav-bar').html(data);
     });
 }
 
 if($('#Footer').html() == "") {
-    $.get("Fragments/footer.html", function(data){
+    $.get("footer.html", function(data){
         data = data.replace(/^.*?<div><\/div>(.*?)<div><\/div>.*?$/s, "$1");
         $("#Footer").html(data);
     });
