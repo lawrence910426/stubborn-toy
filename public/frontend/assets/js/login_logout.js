8ed10ea01669527a0e8f5f6dc5687a81cbee3b0b
lawrence910426 <lawrence910426@gmail.com>
2020-07-26 17:03:09 +0800

    more tests

--- public/frontend/assets/js/login_logout.js
+++ public/frontend/assets/js/login_logout.js
@@ -1,11 +1,24 @@
-/*
-FB.login(function() {
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
+$(document).ready(function() {
+    function get_fields() {
+        return Promise(function(result, reject) {
             FB.api('/me', function(response) {
                 console.log(JSON.stringify(response));
+                result(response);
             });
-        }
-    });
-});
-*/
+        })
+    }
+    function get_connected() {
+        return Promise(function(result, reject) {
+            FB.getLoginStatus(function(response) {
+                result(response.status === 'connected')
+            });  
+        })
+    }
+    $("#login_btn").click(function() {
+        FB.login(async function() {
+            if(await get_connected())
+                console.log(await get_response());
+        }, {scope: 'public_profile,email'});  
+    })
+})
+
