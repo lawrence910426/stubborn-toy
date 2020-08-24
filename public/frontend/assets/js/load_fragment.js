36dde9d5e58c27fe37162528e73f2c556129aa29
lawrence910426 <lawrence910426@gmail.com>
2020-08-24 14:58:00 +0800

    preview

--- public/frontend/assets/js/load_fragment.js
+++ public/frontend/assets/js/load_fragment.js
@@ -1,15 +1,3 @@
-if($('#Nav-bar').html() == "") {
-    const startkey = '<div id="nav">';
-    const endkey = '<div id="nav_end"></div>';
-    $.get("nav.html", function(data){
-        var content = data.substring(
-            data.lastIndexOf(startkey), 
-            data.lastIndexOf(endkey)
-        );
-        $('#Nav-bar').html(content);
-    });
-}
-
 if($('#Footer').html() == "") {
     $.get("footer.html", function(data){
         data = data.replace(/^.*?<div><\/div>(.*?)<div><\/div>.*?$/s, "$1");
