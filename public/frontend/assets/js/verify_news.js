2451cd403ea81908a48eb43fcc55df045784d00e
lawrence910426 <lawrence910426@gmail.com>
2020-09-29 00:08:58 +0800

    Apply foreach

--- public/frontend/assets/js/verify_news.js
+++ public/frontend/assets/js/verify_news.js
@@ -3,30 +3,32 @@ $(document).ready(function() {
         return `<div class="card" style="margin-bottom: 30px;">
     <div class="card-body">
         <h4 class="card-title">${news.title}</h4>
-        <div class="d-flex d-xl-flex justify-content-center justify-content-xl-center align-items-xl-center" src="${news.normal_image_link}" style="margin-top: 20px;margin-bottom: 20px;"><img /></div><p class="card-text"> ${news.category} ${news.datetime} ${news.is_headline ? " #頭條 " : ""} ${news.is_hot ? " #熱門 " : ""} ${news.is_interview ? " #專訪 " : ""} </br> ${news.content.slice(0, 100)} </p><a class="card-link" href="#">查看${news.is_advanced == "true" ? "即時" : "普通"}投稿</a></div>
+        <div class="d-flex d-xl-flex justify-content-center justify-content-xl-center align-items-xl-center" src="${news.normal_image_link}" style="margin-top: 20px;margin-bottom: 20px;"><img /></div><p class="card-text"> ${news.category} ${news.datetime} ${news.is_headline ? " #頭條 " : ""} ${news.is_hot ? " #熱門 " : ""} ${news.is_interview ? " #專訪 " : ""} </br> ${news.content.slice(0, 100)} </p><a class="card-link" href="#">查看${news.is_advanced == "1" ? "即時" : "普通"}投稿</a></div>
 </div>`;
     }
     
     var A = ["shown", "not_shown"] 
     var B = ["advanced", "normal"] 
     
-    for(var a in A) for(var b in B) {
-        var criteria = {
-            "advanced": B[b] == "advanced" ? 1 : 0, 
-            "shown": A[a] == "shown" ? 1 : 0, 
-            "paging": { 
-                "offset": 0 * 18, 
-                "limit": 18 
+    A.forEach(function(a) {
+        B.forEach(function(b) {
+            var criteria = {
+                "advanced": b == "advanced" ? 1 : 0, 
+                "shown": a == "shown" ? 1 : 0, 
+                "paging": { 
+                    "offset": 0 * 18, 
+                    "limit": 18 
+                }
             }
-        }
-        $.post(
-            config.host + "get_news", 
-            criteria
-        ).done(function(data) {
-            data = JSON.parse(data)
-            data.forEach((element) => {
-                $(`.${A[a]}.${B[b]}`).append(gen_abstract(element))
-            });
+            $.post(
+                config.host + "get_news", 
+                criteria
+            ).done(function(data) {
+                data = JSON.parse(data)
+                data.forEach((element) => {
+                    $(`.${a}.${b}`).append(gen_abstract(element))
+                });
+            })      
         })
-    }
+    })
 })
