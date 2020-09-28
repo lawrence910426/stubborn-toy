36a1b6171f350963cfda697c0538f65bbb0c5c4f
lawrence910426 <lawrence910426@gmail.com>
2020-09-28 21:37:35 +0800

    withdraw stuff

--- public/frontend/assets/js/personal_info.js
+++ public/frontend/assets/js/personal_info.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    $("#bank_balance").text(`您的帳戶餘額：${window.localStorage.user.balance}$`) 
+    
+    var checked = window.localStorage.user.withdraw == "1" ? "cond_new" : "cond_used"
+    $(`#${checked}`).prop('checked',true);
+    
     $('input[type=radio][name=bike_cond]').change(function() {
         $.post(config.host + "withdraw", 
         {
@@ -7,6 +12,9 @@ $(document).ready(function() {
             window.location.reload()
         })
     });
+    
+    $("#text_bank_id").val(window.localStorage.user.bank_id)
+    $("#text_bank_account").val(window.localStorage.user.bank_account)
     
     $("#save_bank_account").click(function() {
         $.post(config.host + "withdraw", 
