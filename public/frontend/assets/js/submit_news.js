cb0e3947493e6e359ad2b8975528be3e95e05617
lawrence910426 <lawrence910426@gmail.com>
2020-09-28 16:29:02 +0800

    edit news relationship

--- public/frontend/assets/js/submit_news.js
+++ public/frontend/assets/js/submit_news.js
@@ -126,7 +126,7 @@ $(document).ready(function() {
             email: $("#Announce_Email_Address").val()
         }
         ).done(function(data) {
-            window.location.href = "../index.html"
+            window.location.href = "index.html"
         })
     })
 });
