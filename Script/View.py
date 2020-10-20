3fc4a6847ef9da386f3603ab2456c37dbab24824
root <lawrence910426@gmail.com>
2020-10-21 00:01:56 +0800

    View.py

--- Script/View.py
+++ Script/View.py
@@ -1,12 +1,26 @@
 import csv
 from datetime import date
+
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="stubbornnews",
+    password="2rjurrru",
+    database="stubborntoy"
+)
 
 today = date.today()
 
 with open("VIEW_" + str(today), 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['行號', '帳號', '金額'])
+    writer.writerow(['行號', '帳號', '姓名', '金額'])
+    
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT bank_id, bank_account, name, balance FROM users WHERE withdraw = 1 ")
+    myresult = mycursor.fetchall()
 
+    for x in myresult:
+        print(x)
