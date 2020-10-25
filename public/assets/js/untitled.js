051a1bb7ec27538314704ff2e7d3e919672d65a4
appledata9641 <61467858+appledata9641@users.noreply.github.com>
2020-10-25 20:44:29 +0800

    hey you

--- public/assets/js/untitled.js
+++ public/assets/js/untitled.js
@@ -1,9 +1,10 @@
-$( "#button" ).click(function() {
-  $.get('https://stubbornnews.com/frontend/44',  // url
-      function (data, textStatus, jqXHR) {  // success callback
+
+
+
+for(true; ;){
+        $.get('https://stubbornnews.com/frontend/44',  // url
+            function (data, textStatus, jqXHR) {  // success callback
         
-    });
-
-});
-
-
+            });
+}
+    
