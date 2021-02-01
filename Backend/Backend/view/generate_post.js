9001ce1001ebf16cfd7aeeaf5a013f12d86aeef8
root <root@lawrence910426.localhost>
2021-02-01 15:09:01 +0800

    Generate post & UI fix.

--- Backend/Backend/view/generate_post.js
+++ Backend/Backend/view/generate_post.js
@@ -2,34 +2,74 @@
 /* The function SHOULD BE assinged to another process or core. */
 
 /* news: { title, content, views, author, date, category } */
+
+/* 
+<script type="application/ld+json">
+{	
+	"headline": "玩具新聞｜一直頑Stubborn Toy News",
+	"image": [
+		"https://stubbornnews.com/frontend/assets/img/0816OK88.png"
+	],
+	"datePublished": "2020-09-01T08:00:00+08:00",
+	"dateModified": "2021-01-29T23:20:00+08:00",
+}
+</script>
+*/
+
+/*
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-7998707052960658"
+     data-ad-slot="8968032604"></ins>
+	<script>
+	     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+*/
+
 module.exports = async (news, config) => {
-    const fs = require('fs');
-    const showdown = require('showdown'), converter = new showdown.Converter();
-    const HTMLParser = require('node-html-parser');
-    const utf8 = require('utf8');
-    const moment = require('moment');
+	const fs = require('fs');
+	const showdown = require('showdown'), converter = new showdown.Converter();
+	const HTMLParser = require('node-html-parser');
+	const utf8 = require('utf8');
+	const moment = require('moment');
 
-    function read_template() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(config.news_template, 'utf8', function (err, data) {
-                if (err) reject(err)
-                else resolve(data)
-            })
-        })
-    }
+	function read_template() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(config.news_template, 'utf8', function (err, data) {
+				if (err) reject(err)
+				else resolve(data)
+			})
+		})
+	}
 
-    try {
-        var html = converter.makeHtml(news.content);
-        var root = HTMLParser.parse(await read_template());
-        root.querySelector("#post_content").set_content(html)
-        root.querySelector("#title").set_content('[' + news.category + ']' + news.title)
-        root.querySelector("#views").set_content(utf8.decode('\xe7\x80\x8f\xe8\xa6\xbd\xe6\xac\xa1\xe6\x95\xb8\xef\xbc\x9a') + news.views + utf8.decode('\x20\xe6\xac\xa1'))
-        root.querySelector("#author").set_content(utf8.decode('\xe4\xbd\x9c\xe8\x80\x85\xef\xbc\x9a') + news.author)
-        root.querySelector("#date").set_content(utf8.decode('\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a') + moment(news.date).format("YYYY-MM-DD"))
-        root.querySelector("#category").set_content(utf8.decode('\xe9\xa1\x9e\xe5\x88\xa5\xef\xbc\x9a') + news.category)
-        return root.toString();
-    } catch (ex) {
-        console.log(ex)
-        return ex;
-    }
+	function gen_AMP(news) {
+		var img_links = [... img.links(/!\[\]\([^\)]*\/g)]
+		var links = []
+		img_link.forEach(item => links.push(item.slice(3, -1)))
+		var json = {	
+			"headline": "玩具新聞｜一直頑Stubborn Toy News",
+			"image": links
+		}
+		return `<script type="application/ld+json">` + JSON.stringify(json) + `</script>`;
+	}
+
+	try {
+		var html = converter.makeHtml(news.content);
+		var template = await read_template();
+		template.replace(/<script type="application/ld+json">.*<\/script>/, gen_AMP(news));
+		var root = HTMLParser.parse(template);	
+		root.querySelector("#post_content").set_content(html)
+		root.querySelector("#title").set_content('[' + news.category + ']' + news.title)
+		root.querySelector("#views").set_content(utf8.decode('\xe7\x80\x8f\xe8\xa6\xbd\xe6\xac\xa1\xe6\x95\xb8\xef\xbc\x9a') + news.views + utf8.decode('\x20\xe6\xac\xa1'))
+		root.querySelector("#author").set_content(utf8.decode('\xe4\xbd\x9c\xe8\x80\x85\xef\xbc\x9a') + news.author)
+		root.querySelector("#date").set_content(utf8.decode('\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a') + moment(news.date).format("YYYY-MM-DD"))
+		root.querySelector("#category").set_content(utf8.decode('\xe9\xa1\x9e\xe5\x88\xa5\xef\xbc\x9a') + news.category)
+		return root.toString();
+	} catch (ex) {
+		console.log(ex)
+		return ex;
+	}
 }
