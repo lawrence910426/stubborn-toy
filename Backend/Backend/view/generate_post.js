36ae7da13ed236e0582562503b9d524c47fd9663
root <root@instance-1.asia-east1-a.c.shining-sol-307404.internal>
2021-06-04 13:21:02 +0000

    Ready to migarte server

--- Backend/Backend/view/generate_post.js
+++ Backend/Backend/view/generate_post.js
@@ -22,6 +22,7 @@ module.exports = async (news, config) => {
 	const HTMLParser = require('node-html-parser');
 	const utf8 = require('utf8');
 	const moment = require('moment');
+	const cheerio = require('cheerio');
 
 	function read_template() {
 		return new Promise((resolve, reject) => {
@@ -45,20 +46,20 @@ module.exports = async (news, config) => {
 
 	try {
 		var html = converter.makeHtml(news.content);
-		var template = await read_template();
-		var root = HTMLParser.parse(template);			
-		root.querySelector("#post_content").set_content(html)
+		var template = await read_template();	
+		const $ = cheerio.load(template);
+		$("#post_content").append(html)
 		if(news.category == "") {
-			root.querySelector("#title").set_content("<strong>" + news.title + "</strong>")
-			root.querySelector("#category").set_content(utf8.decode('\xe9\xa1\x9e\xe5\x88\xa5\xef\xbc\x9a') + [... news.title.matchAll(/\u{3010}[^\u{3010}]{0,}\u{3011}/gmu)][0]);
+			$("#title").empty().append("<strong>" + news.title + "</strong>")
+			$("#category").empty().append(utf8.decode('\xe9\xa1\x9e\xe5\x88\xa5\xef\xbc\x9a') + [... news.title.matchAll(/\u{3010}[^\u{3010}]{0,}\u{3011}/gmu)][0]);
 		} else {
-			root.querySelector("#title").set_content("<strong>" + utf8.decode('\xe3\x80\x90') + news.category + utf8.decode('\xe3\x80\x91') + news.title + "</strong>")
-			root.querySelector("#category").set_content(utf8.decode('\xe9\xa1\x9e\xe5\x88\xa5\xef\xbc\x9a') + news.category)
+			$("#title").empty().append("<strong>" + utf8.decode('\xe3\x80\x90') + news.category + utf8.decode('\xe3\x80\x91') + news.title + "</strong>")
+			$("#category").empty().append(utf8.decode('\xe9\xa1\x9e\xe5\x88\xa5\xef\xbc\x9a') + news.category)
 		}
-		root.querySelector("#views").set_content(utf8.decode('\xe7\x80\x8f\xe8\xa6\xbd\xe6\xac\xa1\xe6\x95\xb8\xef\xbc\x9a') + news.views + utf8.decode('\x20\xe6\xac\xa1'))
-		root.querySelector("#author").set_content(utf8.decode('\xe4\xbd\x9c\xe8\x80\x85\xef\xbc\x9a') + news.author)
-		root.querySelector("#date").set_content(utf8.decode('\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a') + moment(news.date).format("YYYY-MM-DD"))
-		var output = root.toString()
+		$("#views").empty().append(utf8.decode('\xe7\x80\x8f\xe8\xa6\xbd\xe6\xac\xa1\xe6\x95\xb8\xef\xbc\x9a') + news.views + utf8.decode('\x20\xe6\xac\xa1'))
+		$("#author").empty().append(utf8.decode('\xe4\xbd\x9c\xe8\x80\x85\xef\xbc\x9a') + news.author)
+		$("#date").empty().append(utf8.decode('\xe6\x97\xa5\xe6\x9c\x9f\xef\xbc\x9a') + moment(news.date).format("YYYY-MM-DD"))
+		var output = $.html()
 		output = output.replace(/<script type="application\/ld\+json">[^<]*<\/script>/gm, gen_AMP(news));
 		return output;
 	} catch (ex) {
